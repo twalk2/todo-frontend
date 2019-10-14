8260a35e6c76096cb2c11427506ea5aa656b460d
twalk2 <2twalk2@gmail.com>
2019-10-14 10:31:52 -0600

    initial commit

---
+++ src/todoitem.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: props.done
+    };
+  }
+
+  toggleDone = () => {
+    fetch(`http://localhost:5000/todo/${this.props.id}`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        title: this.props.title,
+        done: !this.state.done
+      })
+    }).then(this.setState({ done: !this.state.done }));
+  };
+
+  render() {
+    return (
+      <div className="todo-item">
+        <input
+          type="checkbox"
+          onChange={this.toggleDone}
+          defaultChecked={this.state.done}
+        />
+        <p className={this.state.done && "done"}>{this.props.title}</p>
+        <button
+          className="delete"
+          onClick={() => this.props.delete(this.props.id)}
+        >
+          X
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
